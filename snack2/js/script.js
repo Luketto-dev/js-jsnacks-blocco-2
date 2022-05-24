3c811168030e90b5dc8207b44e1c4d16b02aeb2b
Luketto-dev <luca.francesconi1995@gmail.com>
2022-05-24 19:48:16 +0200

    aggiunta commenti

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -2,23 +2,32 @@
 //Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi,
 //e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
+// creo array nomi
 let listaNomi = ["luca", "Ludovica", "Rita", "Paolo", "Giovanni", "Jacopo"];
+//creo array cognomi
 let listaCognomi = ["Rossi","Blasi","Colonna","Scaccia","Giusti","Pellegrino"]
 
+//dichiaro array vuoto che andrò a popolare con nome e cognome random
 listaNomiCognomi = [];
 
 for (let i = 0; i< 6; i++) {
     
+    // creo numero random per i nomi
     let numeroCasualeNomi = Math.floor(Math.random() * 6) ;
+    //creo numero random peri cognomi
     let numeroCasualeCognomi = Math.floor(Math.random() * 6) ;
 
+    // nome è uguale all elemento che si trova all indice del numerocasualenomi
     let nome = listaNomi[numeroCasualeNomi];
+     // cognome è uguale all elemento che si trova all indice del numerocasualecognomi
     let cognome = listaCognomi[numeroCasualeCognomi]
 
+    // popolo l array concatenando variabili nome + cognome 
     listaNomiCognomi.push(nome + ' ' + cognome)
 
 }
 
+// stampo l array popolato
 console.log(listaNomiCognomi);
 
 
--- snack3/js/script.js
+++ snack3/js/script.js
@@ -8,15 +8,14 @@ let sommaNumeri = 0
 for (let i = 0; i < 10; i++) {
     numeroCasuale = Math.floor(Math.random() * 100) ;
     listaNumeri.push(numeroCasuale)
-
-    console.log(listaNumeri);
     
-
     if (i % 2 !== 0) {
         sommaNumeri += numeroCasuale
     }
- 
+
 }
+
+console.log(listaNumeri);
 
 console.log(sommaNumeri);
 
