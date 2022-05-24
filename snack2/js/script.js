9a4a9b2bf5bdf819b93fd940a35cf560415bee28
Luketto-dev <luca.francesconi1995@gmail.com>
2022-05-24 15:13:02 +0200

    inizio snack2

---
+++ snack2/js/script.js
@@ -0,0 +1,23 @@
+//Snack2
+//Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi,
+//e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+let listaNomi = ["luca", "Ludovica", "Rita", "Paolo", "Giovanni", "Jacopo"];
+let listaCognomi = ["Rossi", "Blasi","Colona","Scaccia","Giusti","Pellegrino"]
+
+listaNomiCognomi = [];
+
+for (let i = 0; i< 6; i++) {
+
+    let nome = listaNomi[i]
+    let cognome = listaCognomi[i]
+    
+
+    let numeroCasualeNomi = Math.floor(Math.random() * 7) ;
+    let numeroCasualeCognomi = Math.floor(Math.random() * 7) ;
+
+    
+
+    
+}
+
